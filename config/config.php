fe04865ac2b802dee2c432f348d92272595d8915
Eric <eric.cardin@rechargebilling.com>
2013-04-04 22:40:46 -0700

    Turn errors off

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-	error_reporting(E_ALL ^ E_NOTICE);
-	ini_set('display_errors', '1');
+	//error_reporting(E_ALL ^ E_NOTICE);
+	//ini_set('display_errors', '1');
 	
     $config = array();
     
